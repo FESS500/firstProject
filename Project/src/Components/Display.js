fcf8a5ff68bb469537d84771a7097c825298a976
FESS500 <fess500@mail.ru>
2023-10-14 11:45:37 +1000

    Create Display.js

---
+++ Project/src/Components/Display.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from '../Images/logo.png'
+import nav from '../Images/nav.svg'
+import phone from '../Images/phone.svg'
+
+const Display = () => {
+    return (
+      <div className='background'>
+        <div className='header'>
+          <div className='sweetBox'>
+              <img className='logo' src={logo} alt="logo" />
+              Sweet Bunny House
+          </div>
+          <div className='place'>
+              <img src={nav} alt="nav" />
+              г.Хабаровск <br />ул. Рабочий городок д.6
+          </div>
+          <div className='phone'>
+              <img src={phone} alt="phone" />
+              8-999-615-3826
+          </div>
+        </div>
+          
+        <div className='MainHeader'>
+          <div className='buttonWhite'>
+            ВКУСНЕЙШИЕ
+          </div>
+          <div className='textCake'>
+            Пирожные и торты<br />
+            от 1700р/кг с доставкой<br />
+            по Хабаровску
+          </div>
+          <div className='textCold'>
+            Приготовим к назначенному сроку<br />
+            Доставка на авто в холодильнике.
+          </div>
+        <div>
+            <div>
+              Перейти в каталог
+            </div>
+            <div>
+              10 видов начинок на выбор
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+export default Display;
