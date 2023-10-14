a6aac497a0eafdaac703cf54757cece34935f187
FESS500 <fess500@mail.ru>
2023-10-14 11:45:32 +1000

    Create App.js

---
+++ Project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Main from './Components/Main';
+import Display from './Components/Display';
+
+
+const App = () => {
+  return (
+    <div>
+      <Display />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
