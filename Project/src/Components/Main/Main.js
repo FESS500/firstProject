33fb243d26ef62204a67d70bc0c69273aa044006
FESS500 <fess500@mail.ru>
2023-10-17 23:19:03 +1000

    Update Main.js

--- Project/src/Components/Main/Main.js
+++ Project/src/Components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import main from './Main.module.css';
+import cupcakes from '../../Images/cupcakes.png';
 const Main = () => {
     return (
       <div className={main.background}>
@@ -7,13 +8,16 @@ const Main = () => {
           <div className={main.title}>
             Для любых событий и дорогих вам людей{/*Верхний заголовок Main*/}
           </div>
-          <div>{/*Основное тело Main*/}
-            <div>{/*один товар*/}
-              <div>{/*картинка товара*/}
-
+          <div className={main.mainFull}>{/*Основное тело Main*/}
+            <div >{/*один товар*/}
+              <div className={main.productPicture}>{/*картинка товара*/}
+                <img src={cupcakes} alt="cupcakes"/>
               </div>
-              <div>{/*описание товара*/}
-
+              <div className={main.overview}>{/*описание товара*/}
+                  <div className={main.creme}>Кремовый замок</div>
+                  <div className={main.cremecolor}>Нежный крем любого цвета<br />на выбор, вафельная основа</div>
+                  <div className={main.price}>150р/шт.</div>
+                  <div className={main.buttonprice}>Заказать</div>
               </div>
             </div>
           </div>
